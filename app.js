d4d3a007f3527194869a81b9b5c03a50f5e38793
mateguar <luismateobeltranre@gmail.com>
2026-07-23 17:01:21 -0500

    entorno con variable puerto

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import {configDotenv} from "dotenv"
+configDotenv()
 const app = express();  
-const port = 3000; 
+const port = process.env.PORT || 3000; 
 app.get("/", (_, res) => { 
 res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
 });  
 app.listen(port, () => { 
-console.log( `Servidor en funcionamiento en el puerto:  `); 
+console.log( `Servidor en funcionamiento en el puerto ${port}  `); 
 });
