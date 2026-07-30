b8b1321346b1687ccdc88b705d0acd6d80299926
mateguar <luismateobeltranre@gmail.com>
2026-07-30 13:42:27 -0500

    endoint de productos funcionando, config bodyparser

---
+++ README.md
@@ -0,0 +1,9 @@
+1. instalar primero en la terminal :
+npm install express
+npm install express –save
+ 
+2. luego ejecuta:
+ npm run dev
+
+el port es:
+http://localhost:3000
--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import {configDotenv} from "dotenv"
+import bodyparser from "body-parser";
+
 configDotenv()
+
 const app = express();  
 const port = process.env.PORT || 3000; 
+
+//configurar el uso del body-parser para nuestra aplicacion
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/", (_, res) => { 
 res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
 });  
+
+//otro endpoint, funcion de flecha
+app.get("/productos",(req,res)=>{
+    //usando template string ``
+    res.send(`<h1>listado de productos</h1>
+        <ol>
+        <li>televisor</li>
+        <li>celular</li>
+        <li>impresora</li>
+        </ol>`)
+
+})
+
 app.listen(port, () => { 
 console.log( `Servidor en funcionamiento en el puerto ${port}  `); 
 });
