551bd3c782c324c42be3d21aebc45f00ebb40269
santicaro11sep-sketch <santicaro11sep@gmail.com>
2026-08-06 16:15:31 -0500

    creacion ruta uno , commonjs, control de versiones

---
+++ README.md
@@ -0,0 +1,7 @@
+en el terminal poner :
+npm install
+npm install express
+npm dotenv
+
+para corre el servidor :
+node app.js
--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+require ("dotenv").config();
 const express = require('express');  
 const app = express();  
-const port = 3000; 
+const port =  process.env.PORT || 3000; 
+
+// Ruta principal (Home) - Devuelve texto plano
 app.get("/", (_, res) => { 
-res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
+  res.send("Hola , estamos aprendiendo express con la ficha 3407184"); 
 });  
+
+// Nueva ruta /datos - Devuelve información en formato JSON
+app.get("/datos", (_, res) => {
+  res.json({
+    datos_personales: {
+      nombre: "Duban Alejandro",
+      apellido: "Arias Bejarano",
+      listatelefono: [3005329678]
+    },
+    datos_programa: {
+      nombre_programa: "ADSO",
+      tipo_programa: "tecnologo",
+      ficha: "3407184"
+    }
+  });
+});
+
+// Inicialización del servidor 
 app.listen(port, () => { 
-console.log( `Servidor en funcionamiento en el puerto:  `); 
+  // Se añade la variable 'port' usando template literals para ver el número 3000 en la terminal
+  console.log(`Servidor en funcionamiento en el puerto: ${port}`); 
 });
